203eea268673c7e4b98e07d8608f3de85013a449
Ricardo Lobo <ricardolobo@audienciazero.org>
2019-09-30 18:27:02 +0100

    adds renderer keyword argument to widget render as required by django 2.1+

--- plugs_media/widgets.py
+++ plugs_media/widgets.py
@@ -1,16 +1,9 @@
-"""
-Media Widgets
-"""
-
 from django.conf import settings
 from django.forms.widgets import ClearableFileInput
 
 class MediaWidget(ClearableFileInput):
-    """
-    Media widget to deal with images
-    """
 
-    def render(self, name, value, attrs=None):
+    def render(self, name, value, attrs=None, renderer=None):
         output = super(MediaWidget, self).render(name, value, attrs)
         img_html = '<img src="{0}{1}"/><br/>'.format(settings.MEDIA_URL, value)
         return img_html + output
